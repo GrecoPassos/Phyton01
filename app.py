4879f500b6f25e186812287b5d894ff951951430
Grc <grecopassos@gmail.com>
2025-05-09 19:05:27 -0300

    Logout

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin, login_user, login_manager, login_required
+from flask_login import UserMixin, login_user, login_manager, login_required, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -48,7 +48,11 @@ def login():
         return jsonify({"message": "Invalid credentials"}), 401
         
     
-
+@app.route('/logout', methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout successful"})
 
 
 @app.route('/api/products/add', methods=["POST"])
