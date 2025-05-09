14c32f870d3fbcb21740a2384a333682e798986e
Grc <grecopassos@gmail.com>
2025-05-09 16:32:03 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+
+db = SQLAlchemy(app)
+
+# Modelagem 
+# Produto (Id, Name, Price, Description)
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text)
+
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+    # Lógica para adicionar um produto
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data["name"], price=data["price"], description=data.get("description", "" ))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product added successfully"})
+    return jsonify({"message": "Invalid product data"}), 400
+
+@app.route("/api/products/delete/<int:produtc_id>", methods=['DELETE'])
+def delete_product(produtc_id):
+    product = Product.query.get(produtc_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({"message": "Product deleted successfully"})
+    return jsonify({"message": "Product not found"}), 404
+
+@app.route("/api/products/<int:product_id>", methods=['GET'])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({ 
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        })
+    return jsonify({"message": "Product not found"}), 404
+
+# Definir uma rota raiz (pagina inicial) e a função q sera executada ao requisitar
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
